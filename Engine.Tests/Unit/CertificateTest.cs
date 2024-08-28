8b6d989067fe898824ff8a92bdf07533b43731d7
Fred George <fredgeorge@acm.org>
2024-08-28 08:12:30 +0530

    Fred: Cleaning up skeleton

---
+++ Engine.Tests/Unit/CertificateTest.cs
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023 by Fred George
+ * @author Fred George  fredgeorge@acm.org
+ * Licensed under the MIT License; see LICENSE file in root.
+ */
+
+namespace Engine.Tests.Unit;
+
+// Ensures that Certificate works correctly
+public class CertificateTest {
+    
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# engine_template_csharp
+### certificates_csharp
 
-Skeleton for starting a new C# project
+Copyright (c) 2023 by Fred George  
+author: Fred George  fredgeorge@acm.org  
+Licensed under the MIT License; see LICENSE file in root.
